8d22309cdc9cd5508ae3c6cc2f1a053a288a4bad
vinayakshahdeo <vinayak.shahdeo.9@gmail.com>
2021-06-25 21:48:11 +0530

    added skeleton code

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const fs = require('fs');
+const readLine = require('readline');
+
+const commandLineInputs = process.argv;
+
+console.log("working")
+const app = express();
+module.exports = app;
